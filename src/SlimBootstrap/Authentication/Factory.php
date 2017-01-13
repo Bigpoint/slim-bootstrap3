6bcbbbc8fba15f5aa2aa15ea0409a54f44cce2b5
segaja <webmaster@segaja.de>
2017-01-13 22:54:18 +0100

    move authentication config values to sub-keys; validate that authentication config is present

--- src/SlimBootstrap/Authentication/Factory.php
+++ src/SlimBootstrap/Authentication/Factory.php
@@ -43,11 +43,22 @@ class Factory
 
     /**
      * @return SlimBootstrap\Authentication\Oauth
+     *
+     * @throws SlimBootstrap\Exception if "oauth" config is invalid
      */
     public function createOauth(): SlimBootstrap\Authentication\Oauth
     {
+        if (false === \array_key_exists('oauth', $this->config)
+            || false === \is_array($this->config['oauth'])
+            || false === \array_key_exists('authenticationUrl', $this->config['oauth'])
+            || false === \is_string($this->config['oauth']['authenticationUrl'])
+            || true === empty($this->config['oauth']['authenticationUrl'])
+        ) {
+            throw new SlimBootstrap\Exception('"oauth" config invalid');
+        }
+
         return new SlimBootstrap\Authentication\Oauth(
-            $this->config['authenticationUrl'],
+            $this->config['oauth']['authenticationUrl'],
             $this->httpCaller,
             $this->logger
         );
@@ -55,12 +66,26 @@ class Factory
 
     /**
      * @return SlimBootstrap\Authentication\Jwt
+     *
+     * @throws SlimBootstrap\Exception if "jwt" config is invalid
      */
     public function createJwt(): SlimBootstrap\Authentication\Jwt
     {
+        if (false === \array_key_exists('jwt', $this->config)
+            || false === \is_array($this->config['jwt'])
+            || false === \array_key_exists('providerUrl', $this->config['jwt'])
+            || false === \is_string($this->config['jwt']['providerUrl'])
+            || true === empty($this->config['jwt']['providerUrl'])
+            || false === \array_key_exists('claims', $this->config['jwt'])
+            || false === \is_string($this->config['jwt']['claims'])
+            || 0 === \count($this->config['jwt']['claims'])
+        ) {
+            throw new SlimBootstrap\Exception('"jwt" config invalid');
+        }
+
         return new SlimBootstrap\Authentication\Jwt(
-            $this->config['providerUrl'],
-            $this->config['claims'],
+            $this->config['jwt']['providerUrl'],
+            $this->config['jwt']['claims'],
             $this->httpCaller,
             $this->logger
         );
